083c5ebf99792c711a6a130833942ba8518ff743
Richie <richiemugambi4@gmail.com>
2021-07-09 23:26:12 -0700

    added package.json

---
+++ js/index.js
@@ -0,0 +1,8 @@
+import {init} from 'ityped' ;
+const heythere= document.querySelector('#type-animation');
+init(heythere, {
+     showCursor: true,
+     strings: ['Hey there'],
+     typeSpeed:  100,
+     backDelay:  500,
+    })
