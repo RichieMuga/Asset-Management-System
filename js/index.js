4ca4f919cbf952bf7dddcf729d734a630192d47a
Richie <richiemugambi4@gmail.com>
2021-07-28 05:35:42 -0700

    added logic to right slider buttton

--- js/index.js
+++
@@ -1,8 +0,0 @@
-import {init} from 'ityped' ;
-const heythere= document.querySelector('#type-animation');
-init(heythere, {
-     showCursor: true,
-     strings: ['Hey there'],
-     typeSpeed:  100,
-     backDelay:  500,
-    })
