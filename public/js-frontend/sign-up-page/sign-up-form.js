c3de2c3a2433efe75e51b4d2ce3a086daaf54bd2
Richie Mugambi <richiemugambi4@gmail.com>
2021-11-03 14:33:16 +0300

    adding fetch api for sign up in jsfrontend folder

--- public/js-frontend/sign-up-page/sign-up-form.js
+++ public/js-frontend/sign-up-page/sign-up-form.js
@@ -1,13 +1,20 @@
-      const form= document.getElementsByClassName("sign-up-form")
-      const formData=new FormData(form)
-      form.addEventListener('submit',async (e)=>{
-          e.preventDefault()
-        const response = await fetch('/api/v1/signup',{
-            method:'POST',
-            headers:{'content-type':'application/json'},
-            body:{formData}
-        }
-        )
-        const result= await response.json()
-        return result;
-      })
+const form = document.getElementById('form-sign-up')
+let formData = new FormData(form)
+form.addEventListener('submit', async (e) => {
+  e.preventDefault()
+  console.log(formData)
+  const response = await fetch('/api/v1/auth', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'content-type': 'application/json'
+    },
+    body: { formData }
+  })
+    .catch(error => {
+      console.log(error);
+    })
+  const result = await response.json()
+  console.log(result)
+  return result;
+})
